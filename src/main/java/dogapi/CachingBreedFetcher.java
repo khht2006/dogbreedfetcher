6bb5bccc62693dabbe77dfb7000d99d40d464dec
Kyle Hu <khht2006@gmail.com>
2025-10-18 00:27:37 -0400

    task 1

--- src/main/java/dogapi/CachingBreedFetcher.java
+++ src/main/java/dogapi/CachingBreedFetcher.java
@@ -12,7 +12,7 @@ public class CachingBreedFetcher implements BreedFetcher {
     }
 
     @Override
-    public List<String> getSubBreeds(String breed) {
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
         // If already cached, return directly (no new API call)
         if (cache.containsKey(breed)) {
             return cache.get(breed);
